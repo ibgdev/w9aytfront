8b4aa400a3bf625d04fc7414a6cd2d0bb814f787
Mayssabenchikh <abderrahmenmayssa@gmail.com>
2025-11-18 22:45:20 +0100

    kpis

--- src/app/home/hero/hero.ts
+++ src/app/home/hero/hero.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService, User } from '../../core/services/auth.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
 export class Hero {
 
+ private auth = inject(AuthService);
+   isloggedIn (): boolean{
+    return this.auth.isLoggedIn();
+  }
+   get currentUser(): User | null {
+      return this.auth.getCurrentUser();
+    }
 }
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
 import { Hero } from "./hero/hero";
 import { Solutions } from "./solutions/solutions";
 import { HowItWorks } from "./how-it-works/how-it-works";
 import { ContactUs } from "./contact-us/contact-us";
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../core/services/auth.service';
+import { KpiComponent } from '../kpi/kpi';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Footer, Hero, Solutions, HowItWorks, ContactUs],
+  imports: [Navbar, Footer, Hero, Solutions, HowItWorks, ContactUs, RouterLink, RouterLinkActive, CommonModule, KpiComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-
+  private auth = inject(AuthService);
+  
+  isloggedIn(): boolean {
+    return this.auth.isLoggedIn();
+  }
 }
